2cd20b9dcfaff36888c354c66c7edcd7d26f25ef
Petipois <petitpois24@gmail.com>
2025-09-05 01:09:22 +0200

    ui updates

---
+++ src/lib/consts.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        label: "Home",
+        link: "/",
+    },
+    {
+        label: "How It Works",
+        link: "/#how-it-works",
+    },
+    {
+        label: "Features",
+        link: "/#features",
+    },
+    {
+        label: "Convert",
+        link: "/converter",
+    },
+];
