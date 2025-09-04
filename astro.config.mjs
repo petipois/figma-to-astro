f43f938ef5d8e6822e2d3efbdfaec6352c328e02
Petipois <petitpois24@gmail.com>
2025-09-05 00:51:31 +0200

    added adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	output:"server",
-	vite: {
-		plugins: [tailwindcss()],
+  output:"server",
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
